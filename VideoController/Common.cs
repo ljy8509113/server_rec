5f650847e0439c3fb12ec5def0d7b698b14cd309
ljy8509113 <31403803+ljy8509113@users.noreply.github.com>
2018-02-18 20:12:21 +0900

    lee
    시작할때 인터넷에서 키허용 확인하게 수정함(임시)

--- VideoController/Common.cs
+++ VideoController/Common.cs
@@ -13,6 +13,8 @@ namespace VideoController
         public static string KEY_FTP_PATH = "ftp_path";
         public static string KEY_SETTING_PATH = "setting_path";
         public static string SETTING_PATH = "./setting.txt";
+        public static string KEY_FILE = "./KEY.txt";
+        public static string KEY_URL = "http://ec2-13-125-50-42.ap-northeast-2.compute.amazonaws.com:8080/BoardGame/Key.do";
 
         public static string FTP_PATH = "";
     }
--- VideoController/Program.cs
+++ VideoController/Program.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Text;
 
 namespace VideoController
 {
@@ -14,12 +20,35 @@ namespace VideoController
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            HttpWebRequest myReq = (HttpWebRequest)WebRequest.Create(Common.KEY_URL);
+            myReq.Method = "GET"; // 필요 없는듯?
             
-        }
+            //HttpWebResponse 객체 받아옴
+            HttpWebResponse wRes = (HttpWebResponse)myReq.GetResponse();
+            
+            // Response의 결과를 스트림을 생성합니다.
+            Stream respGetStream = wRes.GetResponseStream();
+            StreamReader readerGet = new StreamReader(respGetStream, Encoding.UTF8);
+            
+            // 생성한 스트림으로부터 string으로 변환합니다.
+            string resultGet = readerGet.ReadToEnd();
 
-        
+            JObject obj = JObject.Parse(resultGet);
+            Console.WriteLine("json : {0}", obj);
+            Console.WriteLine();
+
+            FileInfo keyFile = new FileInfo(Common.KEY_FILE);
+            if (obj["key"].ToString().ToLower().Equals("t"))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new Form1());
+            }
+            else
+            {
+                MessageBox.Show("사용이 만료 되었습니다.");
+                Application.Exit();
+            }
+        }
     }
 }
